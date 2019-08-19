9121de3d800934c662125ce7f1a914538730691c
Subir-Droid <subirdeyabhi@gmail.com>
2019-08-19 16:03:58 +0530

    Added Helper class

---
+++ TestAutothon/TestAutothon/Source/Helper.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by Ranorex
+ * User: Asmi
+ * Date: 8/19/2019
+ * Time: 4:02 PM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+
+namespace TestAutothon
+{
+	/// <summary>
+	/// Description of Helper.
+	/// </summary>
+	public class Helper
+	{
+		public Helper()
+		{
+		}
+	}
+}
