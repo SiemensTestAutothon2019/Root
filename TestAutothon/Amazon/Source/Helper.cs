ac743c556013d66c6e31799bd7543c77b1f10cc1
Subir-Droid <subirdeyabhi@gmail.com>
2019-08-19 16:48:48 +0530

    Added Source folder

---
+++ TestAutothon/Amazon/Source/Helper.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by Ranorex
+ * User: Asmi
+ * Date: 8/19/2019
+ * Time: 4:48 PM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+
+namespace Amazon
+{
+	/// <summary>
+	/// Description of Helper.
+	/// </summary>
+	public class Helper
+	{
+		public Helper()
+		{
+		}
+	}
+}
