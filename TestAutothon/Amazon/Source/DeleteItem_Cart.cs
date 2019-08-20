2d6a0aa704d41074a86e5c94192138d2b1e97406
Subir-Droid <subirdeyabhi@gmail.com>
2019-08-20 11:44:45 +0530

    Added and modified CM

--- TestAutothon/Amazon/Source/DeleteItem_Cart.cs
+++ TestAutothon/Amazon/Source/DeleteItem_Cart.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * Created by Ranorex
  * User: Asmi
- * Date: 8/19/2019
- * Time: 5:11 PM
+ * Date: 8/20/2019
+ * Time: 11:43 AM
  * 
  * To change this template use Tools > Options > Coding > Edit standard headers.
  */
@@ -21,15 +21,15 @@ using Ranorex.Core.Testing;
 namespace Amazon
 {
     /// <summary>
-    /// Description of Click_Cart.
+    /// Description of DeleteItem_Cart.
     /// </summary>
-    [TestModule("26344B4C-2298-4EC0-B2AE-7D5CD0DAA6F7", ModuleType.UserCode, 1)]
-    public class Click_Cart : ITestModule
+    [TestModule("285B4915-97CA-4738-916A-0B140154C3BA", ModuleType.UserCode, 1)]
+    public class DeleteItem_Cart : ITestModule
     {
         /// <summary>
         /// Constructs a new instance.
         /// </summary>
-        public Click_Cart()
+        public DeleteItem_Cart()
         {
             // Do not delete - a parameterless constructor is required!
         }
@@ -45,23 +45,6 @@ namespace Amazon
             Mouse.DefaultMoveTime = 300;
             Keyboard.DefaultKeyPressTime = 100;
             Delay.SpeedFactor = 1.0;
-            
-            Report.Info("Click on cart option in Amazon Home page");
-            
-            AmazonRepository repo = AmazonRepository.Instance;
-			WebElement elmCart;
-			bool clickOperation=true;
-			do
-			{
-				if(!repo.Amazon.Header.CartInfo.Exists(5000, out elmCart))
-					throw new RanorexException("Cart is not avaialable in the website");
-				if(elmCart==null)
-					clickOperation = false;
-				else
-					elmCart.Click();
-				
-			}while(clickOperation==false);
-			Report.Success("Cart has been click successfully");
         }
     }
 }
